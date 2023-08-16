ed3b61c732ba87dd0926c73e45f68b5d36a16edc
naveen_kadampally <naveenkadampally@gmail.com>
2023-08-16 17:43:43 +0100

    data_transformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
+
 
 
 @dataclass
@@ -52,7 +57,11 @@ class DataIngestion:
         
         
 
-if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
             
