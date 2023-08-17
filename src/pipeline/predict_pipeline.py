b27bdb282d92e2309792767f22f54a617da80bb1
naveen_kadampally <naveenkadampally@gmail.com>
2023-08-17 23:21:55 +0100

    debug error corrected

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -10,8 +10,8 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path=os.path.join("artifact","model.pkl")
-            preprocessor_path=os.path.join('artifact','proprocessor.pkl')
+            model_path = r"artifact\model.pkl"
+            preprocessor_path = r'artifact\proprocessor.pkl'
             print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
